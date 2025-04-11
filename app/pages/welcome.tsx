7d2256f7ca40a1ebf5fb19e2bc5d0b4f77e128dc
Joe Heffernan <interim17@gmail.com>
2025-04-11 15:19:48 -0700

    take roast link off main page

--- app/pages/welcome.tsx
+++ app/pages/welcome.tsx
@@ -9,9 +9,6 @@ export default function Welcome() {
         <main className="flex items-center justify-center pt-16 pb-4">
             <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
                 <Header />
-                <Link to="/lambroast">
-                    Lamb Roast
-                </Link>
             </div>
         </main>
     );
